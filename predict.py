4c774c24374aeded16fdc9e9c8bef071ae65d111
Sanjana <sanjana1.aiml24@cmrit.ac.in>
2026-05-12 13:48:58 +0530

    Updated prediction pipeline and trained model

--- predict.py
+++ predict.py
@@ -1,13 +1,39 @@
-from data_loader import download_stock_data
+from tensorflow.keras.models import load_model
+from preprocess import prepare_data
 
-ticker = "AAPL"
-start_date = "2020-01-01"
-end_date = "2023-12-31"
+import pandas as pd
+import numpy as np
 
-df = download_stock_data(ticker, start_date, end_date)
+# Load trained model
+model = load_model("stock_model.h5")
 
-if df.empty:
-    print("Data download failed. Try again.")
-else:
-    print(df.head())
-    print("Shape:", df.shape)
+# Load saved stock data
+df = pd.read_csv("stock_data.csv")
+
+print(df.head())
+print("Shape:", df.shape)
+
+# Prepare data
+X, y, scaler = prepare_data(df)
+
+# Make predictions
+predictions = model.predict(X)
+
+# Convert predictions back to original scale
+dummy_pred = np.zeros((len(predictions), 5))
+dummy_pred[:, 3] = predictions.flatten()
+
+predictions_actual = scaler.inverse_transform(dummy_pred)[:, 3]
+
+# Convert actual values back
+dummy_actual = np.zeros((len(y), 5))
+dummy_actual[:, 3] = y.flatten()
+
+actual_prices = scaler.inverse_transform(dummy_actual)[:, 3]
+
+# Print prediction results
+print("\nPredicted Prices:")
+print(predictions_actual[:5])
+
+print("\nActual Prices:")
+print(actual_prices[:5])
